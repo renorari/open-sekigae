d9def780ef82edc43fa9e9c3055e5c67cd95ed32
renorari <renorari@renorari.net>
2025-05-29 17:23:41 +0900

    🔥 remove: google tags

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -9,7 +9,7 @@ import theme from "../theme.config";
 
 type RootLayoutProps = { children: ReactNode };
 
-const baseUrl = "https://sekigae.renorari.net";
+const baseUrl = process.env.BASE_URL || "https://sekigae.renorari.net";
 
 export default function RootLayout({ children }: RootLayoutProps) {
     const router = useRouter();
@@ -21,19 +21,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="canonical" href={canonicalUrl} />
-                {process.env.NODE_ENV === "production" && (
-                    <>
-                        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VLSW5SRFC0"></script>
-                        <script dangerouslySetInnerHTML={{
-                            "__html": `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-VLSW5SRFC0');
-                        `}} />
-                    </>
-                )}
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1265980632516511" crossOrigin="anonymous"></script>
             </head>
             <body>
                 <CssVarsProvider theme={theme}>
